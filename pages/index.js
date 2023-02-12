51aec5001eb553ccbff47321be04988dc4291438
Drew Neil <andrew.jr.neil@gmail.com>
2023-02-12 17:19:00 +0000

    Redo the tutorial in Feb 2023

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,20 @@
-import Link from 'next/link'
-import Date from '../components/date'
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
-
-export default function Home({allPostsData}) {
+export default function Home() {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I'm Drew Neil. You may recognise my voice from such places as Vimcasts.org</p>
+        <p>Just following the tutorial here.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
-          ))}
-        </ul>
-      </section>
-
     </Layout>
-  )
+  );
 }
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,19 +1,58 @@
-import Link from 'next/link'
-import Head from 'next/head'
-import Layout from '../../components/layout'
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
     <Layout>
-    <Head>
-      <title>First Post</title>
-    </Head>
+      <Head>
+        <title>First post</title>
+      </Head>
       <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
+      <p>
+        I'm baby +1 taxidermy 3 wolf moon 8-bit. Sus chillwave small batch
+        offal, thundercats edison bulb hexagon mumblecore deep v echo park hot
+        chicken irony leggings. Letterpress hell of PBR&B sriracha four loko
+        scenester. Leggings venmo hoodie kitsch hell of palo santo, cornhole
+        fixie actually vape polaroid ennui pour-over adaptogen. +1 four loko
+        gentrify cold-pressed crucifix stumptown slow-carb bushwick. Woke 90's
+        activated charcoal pug, franzen green juice microdosing dreamcatcher
+        biodiesel wayfarers salvia scenester pour-over.
+      </p>
+      <p>
+        Woke shaman tacos, bruh polaroid helvetica banjo. Cray Brooklyn
+        live-edge franzen, DIY kickstarter cloud bread four dollar toast
+        skateboard jianbing street art gluten-free austin lo-fi roof party. Tonx
+        pork belly taiyaki biodiesel, thundercats cred try-hard. Hammock kitsch
+        fixie chicharrones lo-fi artisan. Tote bag cloud bread hoodie gastropub
+        forage listicle, blog beard pabst twee bicycle rights microdosing
+        actually paleo. Tote bag seitan plaid poutine locavore.
+      </p>
+      <p>
+        Mumblecore flannel vaporware gentrify direct trade, bitters seitan retro
+        +1 distillery irony aesthetic subway tile. DIY letterpress before they
+        sold out keffiyeh leggings quinoa. Keffiyeh skateboard snackwave lomo
+        poutine fit put a bird on it. Cold-pressed cred butcher tote bag tousled
+        hoodie trust fund glossier occupy keytar microdosing flannel YOLO
+        gastropub twee.
+      </p>
+      <p>
+        Hell of authentic migas viral organic post-ironic. Food truck cloud
+        bread sus glossier selfies banjo. Forage raw denim chia, occupy jianbing
+        venmo tattooed butcher deep v adaptogen quinoa hell of. Listicle
+        helvetica fingerstache keytar tacos dreamcatcher pinterest chillwave
+        JOMO selvage wolf.
+      </p>
+      <p>
+        Shaman artisan locavore tonx mustache seitan ramps pitchfork put a bird
+        on it gochujang small batch hexagon pinterest. Pitchfork cronut mukbang
+        taiyaki plaid small batch, fingerstache sartorial mlkshk deep v bruh
+        neutra gochujang. Hoodie pop-up tofu yuccie, seitan biodiesel vice
+        butcher roof party. Viral narwhal microdosing, whatever sartorial
+        selvage jianbing waistcoat williamsburg kale chips. Lomo four dollar
+        toast migas, cliche blue bottle meggings cray.
+      </p>
+      <p>Dummy text? More like dummy thicc text, amirite?</p>
     </Layout>
-  )
+  );
 }
